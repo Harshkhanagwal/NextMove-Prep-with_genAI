ea4f780d9b2a11f6f261c8c1dd782d925e4083ff
harsh khanagwal <harshkhanagwal@harshs-MacBook-Air.local>
2026-03-27 22:16:03 +0530

    .cjs update

---
+++ server/api/index.cjs
@@ -0,0 +1,11 @@
+let cachedHandler; 
+
+module.exports = async (req, res) => {
+  if (!cachedHandler) {
+    const app = require("../src/app.js");
+    await app.initApp();
+    cachedHandler = app;
+  }
+
+  return cachedHandler(req, res);
+};
